d51a96a6448ad964f68031b13fc8e730f7de3143
Abdulrhman-Bahaa <abdulrhman.bahaa85@gmail.com>
2024-02-17 23:45:21 +0200

    Add timer0 overflow and pin change interrupts

---
+++ atmega328p/MCAL/interrupts/internal/mcal_internal_interrupts_isr.c
@@ -0,0 +1,28 @@
+/**
+ ******************************************************************************
+ * @file       mcal_internal_interrupts_isr.c
+ * @author     Abdulrhman Bahaa
+ * @brief      This source file contains the implementations for char interrupts
+ *             interfaces
+ * @date       2023-12-10
+ ******************************************************************************
+*/
+#include "mcal_internal_interrupts_isr.h"
+
+/* Variables Definitions -----------------------------------------------------*/
+
+/* Functions Implementations -------------------------------------------------*/
+ISR(TIMER0_OVF_vect) {
+    timer0_isr();
+}
+
+
+void timer0_isr(void){
+    if (NULL != timer_isr_app_ptr[0]) {
+       timer_isr_app_ptr[0]();
+    }
+    else {
+        /* Nothing */
+    }
+    TIMER0_INTERRUPT_FLAG_CLEAR();
+}
