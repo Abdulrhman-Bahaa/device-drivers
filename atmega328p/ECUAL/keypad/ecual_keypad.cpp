e8019637805e362871080e704b4691518a2e27af
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2024-03-21 15:45:19 +0200

    Add keypad module

---
+++ atmega328p/ECUAL/keypad/ecual_keypad.cpp
@@ -0,0 +1,82 @@
+ /**
+ ******************************************************************************
+ * @file       ecual_led.h
+ * @author     Abdulrhman Bahaa
+ * @brief      This header file contains the implementations for keypad interfaces
+ * @date       2024-3-15
+ ******************************************************************************
+*/
+#include "ecual_keypad.h"
+
+/* Variables Definitions -----------------------------------------------------*/
+static const uint8_t keys[KEYPAD_ROWS_NUMBER][KEYPAD_COLUMNS_NUMBER] = {
+{'1','2','3'},
+{'4','5','6'},
+{'7','8','9'},
+{'*','0','#'}
+};
+
+/* Functions Implementations ------------------------------------------------*/
+Std_ReturnType ecual_keypad_init(const keypad_config_t* keypad) {
+    Std_ReturnType ret = E_OK;
+    if(NULL == keypad) {
+        ret = E_NOT_OK;
+    }
+    else {
+        for (uint8_t column_num = 0; column_num < KEYPAD_COLUMNS_NUMBER; column_num++) {
+            ret |= mcal_gpio_pin_direction_write(&(keypad->columns[column_num]), OUTPUT); 
+            ret |= mcal_gpio_pin_logic_write(&(keypad->columns[column_num]), LOW);
+        }
+        for (uint8_t row_num = 0; row_num < KEYPAD_ROWS_NUMBER; row_num++) {
+            ret |= mcal_gpio_pin_direction_write(&(keypad->rows[row_num]), INPUT); 
+            ret |= mcal_gpio_pin_logic_write(&(keypad->rows[row_num]), HIGH);
+        }
+    }
+    return ret;
+}
+
+Std_ReturnType ecual_keypad_char_read(const keypad_config_t* keypad, uint8_t* received_char) {
+    Std_ReturnType ret = E_OK;
+    if (NULL == keypad) {
+        ret = E_NOT_OK;
+    }
+    else {
+        static bool prev_btn_pressed = false;
+        logic_t pin_logic = LOW;
+        uint8_t counter = KEYPAD_COLUMNS_NUMBER * KEYPAD_ROWS_NUMBER;
+        uint8_t columns_logic = 0x00;
+
+        for (uint8_t column_num = 0; column_num < KEYPAD_COLUMNS_NUMBER; column_num++) {
+            columns_logic = 0xff;
+            CLEAR_BIT(columns_logic, column_num);
+            ret |= mcal_gpio_pin_logic_write(&(keypad->columns[0]), READ_BIT(columns_logic, 0));
+            ret |= mcal_gpio_pin_logic_write(&(keypad->columns[1]), READ_BIT(columns_logic, 1));
+            ret |= mcal_gpio_pin_logic_write(&(keypad->columns[2]), READ_BIT(columns_logic, 2));
+            for (uint8_t row_num = 0; row_num < KEYPAD_ROWS_NUMBER; row_num++) {  
+                ret |= mcal_gpio_pin_logic_read(&(keypad->rows[row_num]), &pin_logic);
+                if (LOW == pin_logic) {
+                    if (false == prev_btn_pressed) {
+                        *received_char = keys[row_num][column_num];      
+                    }          
+                    else {
+                        /* Nothing */
+                    }             
+                    prev_btn_pressed = true;
+                    column_num = KEYPAD_COLUMNS_NUMBER;
+                    row_num = KEYPAD_ROWS_NUMBER;
+                }
+                else {
+                    counter--;
+                }   
+            }
+        }
+        if (0 == counter) {
+            prev_btn_pressed = false;
+        }
+        else {
+            /* Nothing */
+        } 
+    }
+    return ret;
+}
+
