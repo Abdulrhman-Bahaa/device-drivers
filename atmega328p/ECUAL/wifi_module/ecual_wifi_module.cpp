77035f716a73fdd1a0e0c4461b86c26eef821451
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2024-04-27 14:52:09 +0300

    Update wifi module

--- atmega328p/ECUAL/wifi_module/ecual_wifi_module.cpp
+++ atmega328p/ECUAL/wifi_module/ecual_wifi_module.cpp
@@ -128,4 +128,64 @@ Std_ReturnType ecual_wifi_module_ubidots_data_send(const char* token, const char
     return ret;
 }
 
+Std_ReturnType ecual_wifi_module_ubidots_variable_send(const char* token, const char* device_label, const char* var_label, const char* var_value) {
+    Std_ReturnType ret = E_OK;
+    uint8_t tcp_connection[60] =  "AT+CIPSTART=\"TCP\",\"", json_string[30] = {"{\""}, data_length_str[5], var_value_str[10], content_length_str[3], data_length = 139, content_length = 0,
+    host[] = "industrial.api.ubidots.com", target_path[30] = "/api/v1.6/devices/";
+
+    strcat(json_string, var_label);
+    strcat(json_string, "\": ");
+    strcat(json_string, var_value);
+    strcat(json_string, "}");
+
+    // Init tcp connection
+    strcat(tcp_connection, host);
+    strcat(tcp_connection, "\",80\r\n");
+    ret |= mcal_usart_transmit_string(tcp_connection);
+    _delay_ms(1000);
+
+    // Concat strings and calc size in bytes
+    strcat(target_path, device_label);
+    strcat(target_path, "/");
+    data_length += strlen(target_path);
+    data_length += strlen(host);
+    data_length += strlen(token);
+    data_length += strlen(json_string);
+    content_length = strlen(json_string);
+    sprintf(data_length_str, "%d", data_length);
+    sprintf(content_length_str, "%d", content_length);
+
+    // Start data transmit
+    ret |= mcal_usart_transmit_string("AT+CIPSEND=");
+    ret |= mcal_usart_transmit_string(data_length_str);
+    ret |= mcal_usart_transmit_string("\r\n");
+    _delay_ms(300);
+
+    ret |= mcal_usart_transmit_string("POST ");
+    ret |= mcal_usart_transmit_string(target_path);
+    ret |= mcal_usart_transmit_string(" HTTP/1.1\r\n");
+
+    ret |= mcal_usart_transmit_string("Host: ");
+    ret |= mcal_usart_transmit_string(host);
+    ret |= mcal_usart_transmit_string("\r\n");
+
+    ret |= mcal_usart_transmit_string("User-Agent: ESP8266/1.0\r\n");
+
+    ret |= mcal_usart_transmit_string("X-Auth-Token: ");
+    ret |= mcal_usart_transmit_string(token);
+    ret |= mcal_usart_transmit_string("\r\n");
+
+    ret |= mcal_usart_transmit_string("Content-Type: application/json\r\n");
+
+    ret |= mcal_usart_transmit_string("Connection: close\r\n");
+
+    ret |= mcal_usart_transmit_string("Content-Length: ");
+    ret |= mcal_usart_transmit_string(content_length_str);
+    ret |= mcal_usart_transmit_string("\r\n\r\n");
+
+    ret |= mcal_usart_transmit_string(json_string);
+    ret |= mcal_usart_transmit_string("\r\n");
+    return ret;
+}
+
 
--- atmega328p/ECUAL/wifi_module/ecual_wifi_module.h
+++ atmega328p/ECUAL/wifi_module/ecual_wifi_module.h
@@ -36,5 +36,6 @@ Std_ReturnType ecual_wifi_module_wifi_connect(const char* wifi_ssid, const char*
 Std_ReturnType ecual_wifi_module_wifi_data_send(void);
 Std_ReturnType ecual_wifi_module_http_request(const char* method, const char* host, const char* target_path);
 Std_ReturnType ecual_wifi_module_ubidots_data_send(const char* token, const char* device_label, const key_value_pair_t array[], uint8_t start_index, uint8_t end_index);
+Std_ReturnType ecual_wifi_module_ubidots_variable_send(const char* token, const char* device_label, const char* var_label, const char* var_value);
 #endif /* ECUAL_WIFI_MODULE_H */
 
