b3e062b966930210d5511dbbac094d63e5bcfc3f
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2025-04-19 17:19:17 +0200

    Update header files for consistency, Fix bit mapping function

--- lib/ecual/oled_display/ecual_oled_display.h
+++ lib/ecual/oled_display/ecual_oled_display.h
@@ -10,10 +10,14 @@
 #define	ECUAL_OLED_DISPLAY_H
 
 /* Includes -----------------------------------------------------------------*/
-#include <i2c/mcal_i2c.h>
+#include <atmega328p/i2c/mcal_i2c.h>
+#include <stddef.h>
 #include "ssd1306_fonts.h"
 #include "ssd1306_images.h"
 #include "ssd1306_gifs.h"
+
+#define F_CPU 16000000UL // For a 16 MHz crystal
+#include <util/delay.h>
 
 /* Macro Declarations -------------------------------------------------------*/
 #define OLED_DISPLAY_DISPLAY_ON                                               0xAF
@@ -39,10 +43,10 @@ typedef struct {
 /* Variables Declarations ---------------------------------------------------*/
 
 /* Functions Declarations ---------------------------------------------------*/
-void single_command_byte(void);
-void command_stream(void);
-void single_data_byte(void);
-void data_stream(void);
+Std_ReturnType single_command_byte(void);
+Std_ReturnType command_stream(void);
+Std_ReturnType single_data_byte(void);
+Std_ReturnType data_stream(void);
 Std_ReturnType ecual_oled_display_addressing_mode(const oled_display_config_t* oled_display, const uint8_t mode);
 Std_ReturnType ecual_oled_display_bit_mapping(const oled_display_config_t* oled_display,
                                                     const uint8_t *array_of_bytes, const uint8_t width,
--- lib/ecual/oled_display/ssd1306_images.h
+++ lib/ecual/oled_display/ssd1306_images.h
@@ -10,7 +10,7 @@
 #define	SSD1306_IMAGES_H
 
 /* Includes -----------------------------------------------------------------*/
-#include <std/mcal_std_types.h>
+#include <stdint.h>
 #include <avr/pgmspace.h>
 
 /* Macro Declarations -------------------------------------------------------*/
