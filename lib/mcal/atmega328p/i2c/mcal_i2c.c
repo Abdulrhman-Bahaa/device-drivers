9b24b877d25cc793afa42dced21f2e791b422fd5
Abdulrhman Bahaa <abdulrhman.bahaa85@gmail.com>
2025-04-19 16:12:43 +0200

    Refactor I2C module: update function signatures, improve pin initialization, and correct macro includes

--- lib/mcal/atmega328p/i2c/mcal_i2c.c
+++ lib/mcal/atmega328p/i2c/mcal_i2c.c
@@ -14,33 +14,64 @@
 /* Functions Implementations -------------------------------------------------*/
 Std_ReturnType mcal_i2c_init(void) {
     Std_ReturnType ret = E_OK;
-        I2C_DISABLE();
-        // Set bit rate 100 kHz
-        TWBR = 72; // Bit rate register value for 100 kHz at 16 MHz CPU clock
-        I2C_ENABLE();
+
+    pin_config_t pin_scl = {
+        .location = {
+            .port = PORTC_INDEX,
+            .pin = PIN4_INDEX
+        },
+        .direction = INPUT,
+        .logic = HIGH
+    };
+
+    pin_config_t pin_sda = {
+        .location = {
+            .port = PORTC_INDEX,
+            .pin = PIN5_INDEX
+        },
+        .direction = INPUT,
+        .logic = HIGH
+    };
+    // Initialize SCL and SDA pins
+    ret |= mcal_gpio_pin_init(&pin_scl);
+    ret |= mcal_gpio_pin_init(&pin_sda);
+
+    I2C_DISABLE();
+    // Set bit rate 100 kHz
+    TWBR = 72; // Bit rate register value for 100 kHz at 16 MHz CPU clock
+    I2C_ENABLE();
     return ret;
 }
 
-Std_ReturnType mcal_i2c_address_trasnmit(const uint8_t slave_address, i2c_master_operation_t master_operation) {
+Std_ReturnType mcal_i2c_address_transmit(const uint8_t slave_address, i2c_master_operation_t master_operation) {
     Std_ReturnType ret = E_OK;
-    mcal_i2c_data_trasnmit(slave_address | master_operation);
+    mcal_i2c_data_transmit(slave_address | master_operation);
     return ret;
 }
 
-Std_ReturnType mcal_i2c_data_trasnmit(const uint8_t data_to_transmit) {
+Std_ReturnType mcal_i2c_data_transmit(const uint8_t data_to_transmit) {
     Std_ReturnType ret = E_OK;
     TWDR = data_to_transmit;
     TWCR = (1<<TWINT) | (1<<TWEN);
     while (!(TWCR & (1<<TWINT)));
+    if (0x28 != (TWSR & 0xF8)) {
+        ret = E_NOT_OK;
+    }
     return ret;
 }
 
-void mcal_i2c_start_condition(void) {
+Std_ReturnType mcal_i2c_start_condition(void) {
+    Std_ReturnType ret = E_OK;
     TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
     while (!(TWCR & (1<<TWINT)));
+    if (0x08 != (TWSR & 0xF8)) {
+        ret = E_NOT_OK;
+    }
+    return ret;
 }
 
-void mcal_i2c_stop_condition(void) {
-    SET_BIT(TWCR, TWSTO);
-    SET_BIT(TWCR, TWINT);
+Std_ReturnType mcal_i2c_stop_condition(void) {
+    Std_ReturnType ret = E_OK;
+    TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+    return ret;
 }
--- lib/mcal/atmega328p/i2c/mcal_i2c.h
+++ lib/mcal/atmega328p/i2c/mcal_i2c.h
@@ -10,9 +10,8 @@
 #define	MCAL_I2C_H
 
 /* Includes -----------------------------------------------------------------*/
-#include "../std/mcal_std_types.h"
-#include "../gpio/mcal_gpio.h"
-#include "../interrupts/internal/mcal_internal_interrupts.h"
+#include <atmega328p/gpio/mcal_gpio.h>
+#include <atmega328p/interrupts/internal/mcal_internal_interrupts.h>
 
 /* Macro Declarations -------------------------------------------------------*/
 #define I2C_ENABLE()    (SET_BIT(TWCR, TWEN))
@@ -34,9 +33,9 @@ typedef struct {
 
 /* Functions Declarations ---------------------------------------------------*/
 Std_ReturnType mcal_i2c_init(void);
-void mcal_i2c_start_condition(void);
-void mcal_i2c_stop_condition(void);
-Std_ReturnType mcal_i2c_address_trasnmit(const uint8_t slave_address, i2c_master_operation_t master_operation);
-Std_ReturnType mcal_i2c_data_trasnmit(const uint8_t data_to_transmit);
+Std_ReturnType mcal_i2c_start_condition(void);
+Std_ReturnType mcal_i2c_stop_condition(void);
+Std_ReturnType mcal_i2c_address_transmit(const uint8_t slave_address, i2c_master_operation_t master_operation);
+Std_ReturnType mcal_i2c_data_transmit(const uint8_t data_to_transmit);
 
 #endif	/* MCAL_I2C_H */
